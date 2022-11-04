2d1a92de9102d80109147c031edb5366735bf49a
hassanrodrigues <hassanrodrigues14@gmail.com>
2022-11-04 16:25:17 -0400

    feat/deply

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,15 +3,12 @@ import "dotenv/config";
 
 const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.HOST,
-  port: 5432,
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
+  url: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false}: false,
   logging: true,
   synchronize: false,
-  entities: ["src/entities/*.ts"],
-  migrations: ["src/migrations/*.ts"],
+  entities:process.env.NODE_ENV === "production"? ["dist/entities/*.js"]: ["src/entities/*.ts"],
+  migrations:  process.env.NODE_ENV === "production" ? ["dist/migrations/*.js"]:["src/migrations/*.ts"],
 });
 
 export default AppDataSource;
