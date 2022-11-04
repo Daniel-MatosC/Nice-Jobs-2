b020a8de0245bd28cbd9767cc4a6267429b42098
hassanrodrigues <hassanrodrigues14@gmail.com>
2022-11-04 17:38:04 -0400

    fix: ajusting code

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,16 @@ import "reflect-metadata";
 import express from "express";
 import servicesRoutes from "./routes/services.routes";
 import categoriesRoutes from "./routes/categories.routes";
+import userRoutes from "./routes/user.routes";
 
 const app = express();
 app.use(express.json());
 
-import servicesRoutes from "./routes/services.routes";
-import categoriesRoutes from "./routes/categories.routes";
+app.use(express.json());
+
+app.use("/users", userRoutes);
+app.use("", servicesRoutes);
+app.use("", categoriesRoutes);
 
 app.use("/testando", (req, res) => {
   res.send("Hello World");
