b3d8125bcc1b8a7b2a913cab0e3c0bc815b60177
hassanrodrigues <hassanrodrigues14@gmail.com>
2022-11-05 18:36:22 -0400

    feat/test seesion and users

---
+++ src/__tests__/mocks/index.ts
@@ -0,0 +1,32 @@
+
+export const mockedUserNotPremium = {
+    name: "hassan",
+    email: "hassan@gmail.com",
+    password: "123456",
+    isPremium: false,
+    isOffering: false,   
+}
+export const mockedUserPremium = {
+    name: "hassan da silva",
+    email: "hassansilva@gmail.com",
+    password: "123456",
+    isPremium: true,
+    isOffering: false,   
+}
+export const mockedUserPremiumAndOffering = {
+    name: "hassan da silva junior",
+    email: "hassansilvajunior@gmail.com",
+    password: "123456",
+    isPremium: true,
+    isOffering: true,   
+}
+
+export const mockedUserLogin  = {
+    email: "joana@mail.com",
+    password: "123456"
+}
+
+export const mockedPremiunLogin  = {
+    email: "hassansilva@gmail.com",
+    password: "123456",
+}
