61ae07010d30d34fa34087d71ba0924b5b04a182
hassanrodrigues <hassanrodrigues14@gmail.com>
2022-11-09 17:02:55 -0400

    feateure: change is done schedule

--- src/routes/schedules.routes.ts
+++ src/routes/schedules.routes.ts
@@ -5,11 +5,13 @@ import createScheduleController from "../controllers/schedules/createSchedule.co
 import listSchedulesController from '../controllers/schedules/lisSchedules.controller';
 import listOneScheduleController from '../controllers/schedules/listOneSchedules.controller';
 import listScheduleByServiceController from '../controllers/schedules/listSchedulesByService.controller';
+import changeIsDoneScheduleController from '../controllers/schedules/changeIsDoneSchedule.controller';
 
 const schedulesRoutes = Router();
 
 schedulesRoutes.post("/", ensureAuth,ensureIsOffering, createScheduleController);
 schedulesRoutes.get("/", ensureAuth, listSchedulesController);
+schedulesRoutes.patch("/:id", ensureAuth, changeIsDoneScheduleController);
 schedulesRoutes.get("/:id", ensureAuth, listOneScheduleController);
 schedulesRoutes.get("/services/:id", ensureAuth,ensureIsOffering, listScheduleByServiceController);
 
